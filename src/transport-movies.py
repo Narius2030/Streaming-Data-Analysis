5b484b05c5e84bb1b023e801fc4b3039054b24e9
Narius2030 <buiducnhan15122003@gmail.com>
2024-09-19 16:37:04 +0700

    feat: update the generator of kafka producer

--- src/transport-movies.py
+++ src/transport-movies.py
@@ -12,46 +12,59 @@ from functions.ElasticHandler import ElasticHandlers
 
 settings = get_settings()
 
+
 def request_movies():
+    # TODO: recieve data from API sources - transformation is optional
     stop_event = False
     for page in range(1,6):
-        # TODO: recieve data from API sources - transformation is optional
-        url = f"https://api.themoviedb.org/3/movie/now_playing?language=en-US&page={page}"
-        headers = {
-            "accept": "application/json",
-            "Authorization": f"Bearer {settings.TMDB_BEARER_TOKEN}"
-        }
-        response = requests.get(url, headers=headers)
-        if page == 5:
-            stop_event = True
-        yield (response.json().get('results'), response.json().get('page'), stop_event)
+        try:
+            url = f"https://api.themoviedb.org/3/movie/now_playing?language=en-US&page={page}"
+            headers = {
+                "accept": "application/json",
+                "Authorization": f"Bearer {settings.TMDB_BEARER_TOKEN}"
+            }
+            response = requests.get(url, headers=headers)
+            data = response.json().get('results')
+            page = response.json().get('page')
+            if page == 5:
+                stop_event = True
+        except Exception as exc:
+            raise Exception(str(exc))
+        yield (data, page, stop_event)
     
 
 def request_tvseries():
+    # TODO: recieve data from API sources - transformation is optional
     stop_event = False
     for page in range(1,6):
-        # TODO: recieve data from API sources - transformation is optional
-        url = f"https://api.themoviedb.org/3/tv/airing_today?language=en-US&page={page}"
-        headers = {
-            "accept": "application/json",
-            "Authorization": f"Bearer {settings.TMDB_BEARER_TOKEN}"
-        }
-        response = requests.get(url, headers=headers)
-        if page == 5:
-            stop_event = True
-        yield (response.json().get('results'), response.json().get('page'), stop_event)
+        try:
+            url = f"https://api.themoviedb.org/3/tv/airing_today?language=en-US&page={page}"
+            headers = {
+                "accept": "application/json",
+                "Authorization": f"Bearer {settings.TMDB_BEARER_TOKEN}"
+            }
+            response = requests.get(url, headers=headers)
+            data = response.json().get('results')
+            page = response.json().get('page')
+            if page == 5:
+                stop_event = True
+        except Exception as exc:
+            raise Exception(str(exc))
+        yield (data, page, stop_event)
 
 
 def write_logs(message, path):
-    # Filter value
+    ## TODO: write data into json
     for _, datas in message.items():
-        for data in datas:
-            # write data into json
-            value = data.value.decode('utf-8')
-            value = json.loads(value)
-            print(value)
-            with open(f"{path}/{value['type']}_{date.today()}_{value['page']}.json", "w", encoding="utf-8") as file:
-                json.dump(value, file, indent=4)
+        try:
+            for data in datas:
+                value = data.value.decode('utf-8')
+                value = json.loads(value)
+                with open(f"{path}/{value['type']}_{date.today()}_{value['page']}.json", "w", encoding="utf-8") as file:
+                    json.dump(value, file, indent=4)
+        except Exception as exc:
+            raise Exception(str(exc))
+
 
 def transport(topic):
     prod_tasks = [
@@ -92,10 +105,10 @@ if __name__=='__main__':
     ## TODO: run transporting
     transport(topic_name)
     
-    ## TODO: data processing functions
-    # handler = ElasticHandlers(
-    #     host=settings.ELASTIC_HOST,
-    #     api_key=settings.FILMS_INDEX_KEY,
-    # )
-    # documents = handler.create_documents(index=topic_name, path="./logs/*.json")
+    # TODO: data processing functions
+    handler = ElasticHandlers(
+        host=settings.ELASTIC_HOST,
+        api_key=settings.FILMS_INDEX_KEY,
+    )
+    documents = handler.create_documents(index=topic_name, path="./logs/*.json")
     # handler.ingest_data(es, "films", documents)
